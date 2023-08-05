f9314975a1fc365b96e12ac0b0fb576bcac679b7
KM ANJU <anju96103@gmail.com>
2023-08-05 08:31:00 +0530

    changes

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
 import answerRoutes from "./routes/Answers.js";
-import connectDB from "./connectMongoDb.js";
+//import connectDB from "./connectMongoDb.js";
 
-dotenv.config();
-connectDB();
+
+//connectDB();
 const app = express();
+dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
